d94e9b89e4d0a2a5be3ed716d56f8457ce7cd301
Lazerx2reem <109528438+Lazerx2reem@users.noreply.github.com>
2024-09-12 20:19:17 +0000

    Simple GUI built

---
+++ DemoViewer.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.awt.geom.*;
+import java.awt.image.BufferedImage;
+
+
+public class DemoViewer{
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        Container pane = frame.getContentPane();
+        pane.setLayout(new BorderLayout());
+
+        // slider to control horizontal rotation
+        JSlider headingSlider = new JSlider(0, 360, 180);
+        pane.add(headingSlider, BorderLayout.SOUTH);
+
+        // slider to control vertical rotation
+        JSlider pitchSlider = new JSlider(SwingConstants.VERTICAL, -90, 90, 0);
+        pane.add(pitchSlider, BorderLayout.EAST);
+
+        // panel to display render results
+        JPanel renderPanel = new JPanel() {
+                public void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D) g;
+                    g2.setColor(Color.BLACK);
+                    g2.fillRect(0, 0, getWidth(), getHeight());
+
+                    // rendering magic will happen here
+                }
+            };
+        pane.add(renderPanel, BorderLayout.CENTER);
+
+        frame.setSize(400, 400);
+        frame.setVisible(true);
+    }
+}
+
+class Vertex {
+    double x;
+    double y;
+    double z;
+    Vertex(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
+
+class Triangle {
+    Vertex v1;
+    Vertex v2;
+    Vertex v3;
+    Color color;
+    Triangle(Vertex v1, Vertex v2, Vertex v3, Color color) {
+        this.v1 = v1;
+        this.v2 = v2;
+        this.v3 = v3;
+        this.color = color;
+    }
+}
